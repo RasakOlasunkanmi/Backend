0a16f8207dbefe0973c70e91c9dffa24b106083f
Rasak Akeem Olasukanmi <olasukanmiakeem816@gmail.com>
2025-10-29 16:09:11 +0100

    Wednesday work

--- users.py
+++ users.py
@@ -1,21 +1,30 @@
 from database import db
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
 from sqlalchemy import text
 import os
 from dotenv import load_dotenv
 import bcrypt
 import uvicorn
+from enum import Enum
+from middleware import create_token, verify_token
 
 load_dotenv()
 
 app = FastAPI(title="Simple App", version="1.0.0")
+
+token_time = int(os.getenv("token_time"))
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
 
 class Simple(BaseModel):
     name: str = Field(..., example="Sam Larry")
     email: str = Field(..., example="sam@email.com")
     password: str = Field(..., example="sam123")
     userType: str = Field (..., example="student")
+    gender: Gender = Field(..., example="female")
     
 @app.post("/signup")
 def signUp(input: Simple):
@@ -32,18 +41,19 @@ def signUp(input: Simple):
         
         
         query = text("""
-            INSERT INTO users (name, email, password)
-            VALUES (:name, :email, :password)
+            INSERT INTO users (name, email, password, userType, gender)
+            VALUES (:name, :email, :password, :userType, :gender)
 
-        """)
+    """)
         
         salt = bcrypt.gensalt()
-        hashedPassword = bcrypt.hashpw(input.password.encode('utf-8'), salt)
+        hashPassword = bcrypt.hashpw(input.password.encode('utf-8'), salt)
         
-        db.execute(query, {"name": input.name, "email": input.email, "password": hashedPassword, "userType": input.userType})
+        db.execute(query, {"name": input.name, "email": input.email, "password": hashPassword, "userType": input.userType, "gender": input.gender})
         db.commit()
+        
         return {"message": "User created successfully",
-                "data": {"name": input.name, "email": input.email, "userType": input.userType}}
+                "data": {"name": input.name, "email": input.email, "userType": input.userType, "gender": input.gender}}
         
     except Exception as e:
         print("Email already exists")    
@@ -68,11 +78,91 @@ def login(input: LoginRequest):
         
         if not verified_password:
             raise HTTPException(status_code=404, detail = "invalid email or password")
+        
+        encoded_token = create_token(details={
+            "id": result.id,
+            "email": result.email,
+            "userType": result.userType
+        }, expiry = token_time)
+        
         return {
-            "Message": "Logged in successfully"
+            "Message": "Login successfully",
+            "token": encoded_token
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail= str(e))
+    
+    
+class courseRequest(BaseModel):
+    title: str = Field(..., example="Backend Course")
+    level: str = Field(..., example="Beginner")
+    
+@app.post("/courses")
+def addcourses(input: courseRequest, user_data = Depends(verify_token)):
+    try:
+        print(user_data)
+        
+        if user_data['userType'] != 'admin':
+            raise HTTPException(status_code=401, detail="You are not authorized to add a course")
+        
+        query = text("""
+        INSERT INTO courses (title, level)
+        VALUES (:level, :title)
+    """)
+        db.execute(query, {"title": input.title, "level": input.level})
+        db.commit()
+        
+        return {"message": "Course added successfully",
+                "data": {"title": input.title, "level": input.level}}
+        
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail = str(e))
+    
+  
+class courseId(BaseModel):
+    courseId: int = Field(..., example=1)  
+    
+
+
+@app.post("/enrollment")
+def enrollment(input: courseId, user_data= Depends(verify_token)):
+    print(user_data)
+    check_course = text('''
+                        SELECT * FROM courses WHERE id = :courseId LIMIT 1
+                        ''')
+    result = db.execute(check_course, {"courseId": input.courseId}).fetchone()
+    
+    if result :
+        if user_data['userType'] != 'admin':
+            check_enrollment_exist = text('''
+                        SELECT * FROM enrollments WHERE userId = :userId AND courseId = :courseId  LIMIT 1
+                        ''')
+            check_result = db.execute(check_enrollment_exist, {"userId":  user_data['id'], "courseId": input.courseId}).fetchone()
+            if check_result:
+               return {
+                "message": 'You have enrolled for this course before now, just enter class! '
+                }
+            else:
+                query = text("""
+                    INSERT INTO enrollments (userId, courseId)
+                    VALUES (:userId, :courseId)
+                """)
+                db.execute(query, {"userId":  user_data['id'], "courseId": input.courseId})
+                db.commit()
+                return {
+                "data": 'success! '
+                }
+        else:
+            return {
+            "message": 'Only Students can enrollment! '
+            }
+    else:
+         return {
+            "message": 'Invalid course ID! '
+            }
+    
+   
+
 
 if __name__=="__main__":
     uvicorn.run(app, host = os.getenv("host"), port = int(os.getenv("port")))
